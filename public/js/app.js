a4d3ac8ed064e58c9155320d7a53e06311b1d1fe
rsjargon <rsjargon@gmail.com>
2022-12-31 15:35:34 -0500

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+console.log('Client side javascript file is loaded!')
+
+playerScore = 0
+
+function buttonClicked() {
+    playerScore++;
+    button1 = document.getElementById("btn-1");
+    document.getElementById("btn-1").innerHTML = playerScore;
+
+}
