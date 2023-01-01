8b200794d654f4bee1bc7d0582ee679e2097ac61
= <daltonjunkemail@gmail.com>
2023-01-01 13:45:37 -0500

    Added updates and mongodb connection

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
 console.log('Client side javascript file is loaded')
 
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+
+//const connectionURL = 'mongodb+srv://daltonwinc:<password>@cluster0.kaakpiu.mongodb.net/?retryWrites=true&w=majority'
+//const databaseName = 'game-save-data'
+
+//MongoClient.connect()
+
 playerScore = 0
 
 function buttonClicked() {
